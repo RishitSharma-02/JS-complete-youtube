9cc99231ca1cd040924f0c22ec32106ed585d8ac
RISHIT KUMAR SHARMA <rishitsharma50@gmail.com>
2026-02-20 09:33:02 +0000

    strt data types

---
+++ dataTypes-summary.js
@@ -0,0 +1,7 @@
+// primitive 
+// 7 types : String(call by value) ,number , boolean  , null (empty), undefined , symbol ,BigInt 
+
+
+// reference type(non ptimitive )(iske refference apko allocate kiya ja sakta hai )
+// Arrays , Objects , functions 
+
