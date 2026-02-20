11d6bd9697f25d0d1fbebfe2f30110b64c3546f5
RISHIT KUMAR SHARMA <rishitsharma50@gmail.com>
2026-02-20 11:48:33 +0000

    dataTypeCommit

--- dataTypes-summary.js
+++ dataTypes-summary.js
@@ -5,3 +5,28 @@
 // reference type(non ptimitive )(iske refference apko allocate kiya ja sakta hai )
 // Arrays , Objects , functions 
 
+const score = 1000;
+const score1 = 10232.35;
+
+const isLoggedIn = true ;
+const outsideTmp = null;
+let userEmail;
+
+//symbol
+const id= Symbol('123');
+const superId = Symbol('123');
+console.log(id === superId);
+
+const bigNumber = 7061028461;
+const heros = ["shaktiman" , "doga" , "bheem" , "arjun"];
+let myObj ={
+
+    //in the form of key and value pairs
+    name : "rishit",
+    age : 21,
+    height : 5.11,
+}
+
+let myFunc = function(){
+    console.log("hello world");
+} 
