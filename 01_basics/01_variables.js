6d4eeee435d9bd4aadb93a56df86b0d58a0cbc83
RISHIT KUMAR SHARMA <rishitsharma50@gmail.com>
2026-02-20 06:06:53 +0000

    data type complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 144553; //(not changed )
+let accountEmail = "rishitsharma50@gmail.com";  
+var accountPass = "123456789";
+accountCity = "Ranchi";
+let accountState;
+
+// accountID = 5554879;
+
+console.log(accountID);
+accountEmail ="hcA@gmail.com"
+accountPass ="02093029"
+
+accountCity = "bangluru"
+console.table([accountEmail , accountID ,accountPass ,accountCity , accountState])
+/*
+prefer not to use var becz of issue of block and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs not browser 
+
+// code readiblity should be high 
+let name = "rishit" // it is string 
+let age = 47 // it is integer 
+let isLoggedIn = true // it is boolean 
+
+// null in js is standalone value (representation) 
+// undefined (value is not assigned )
+//symbols => unique 
+// object 
+
+console.log(typeof "rishit")
