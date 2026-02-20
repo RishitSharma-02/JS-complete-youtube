6fa9bf3db3d9f02b5a41de857f6b18d23a4a46cc
RISHIT KUMAR SHARMA <rishitsharma50@gmail.com>
2026-02-20 13:13:58 +0000

    upto strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,5 @@
+const name = "rishit";
+const repoCount = " total 50";
+console.log(name + repoCount + "value"); // not good syntax 
+//insted of this we use backticks `` (use placeholder)
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -17,6 +17,8 @@ const id= Symbol('123');
 const superId = Symbol('123');
 console.log(id === superId);
 
+///********************** */
+
 const bigNumber = 7061028461;
 const heros = ["shaktiman" , "doga" , "bheem" , "arjun"];
 let myObj ={
@@ -30,3 +32,27 @@ let myObj ={
 let myFunc = function(){
     console.log("hello world");
 } 
+
+//memory usages
+//Stack(primitive) variable decleare ke bd yaha se hmko copy milta hai yaha changes copy me hoga
+// heap(non primitive) yaha hmko reference milta hai yaha original value me changes hoga
+
+//Example
+let utube = "RishitSharma" // ye jayega stack me 
+let anotherName = utube;
+anotherName = "chaiAndCode";
+console.log(anotherName);
+console.log(utube);
+
+//Example
+let Obj1 = {
+    userEmail : "rishitSharma50@..",
+    age :21
+}
+let obj2 = Obj1;
+obj2.userEmail = "beingSharma@....";
+console.log(Obj1.userEmail);
+console.log(obj2.userEmail);
+
+
+
