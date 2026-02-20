f08e422239fdd2b43df4234c482a58ad4637c59e
RISHIT KUMAR SHARMA <rishitsharma50@gmail.com>
2026-02-20 06:30:23 +0000

    conversion Of data

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = "33";
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let num = "55avv";
+let value2 = Number(num);
+console.log(value2)
+console.log(typeof value2);
+
+
+let num2 = null
+let value3 = Number(num2)
+console.log(value3)
+console.log(typeof value3)
+
+let num3 = undefined
+let value4 = Number(num3);
+console.log(value4)
+console.log(typeof value4);
+
+// 33 is easily converted into no 
+// 33adb is give NaN not a number
+
+let isLoggedIn =  "Rishit"; // 1 is true 0 is false
+// empty string is false and any string is true
+let booleanIsLoggdIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggdIn);
+
+
+let someNumber = 33;
+let StringCon = String (someNumber);
+console.log(typeof StringCon);
+console.log(StringCon);
+
+
+
+
+
